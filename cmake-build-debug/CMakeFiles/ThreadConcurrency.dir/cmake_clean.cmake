a5e79efc7c703f1ac0839a3567aabde4b285dad1
Hieu Khuong <tug60121@temple.edu>
2020-01-16 21:29:28 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ThreadConcurrency.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThreadConcurrency.dir/main.cpp.o"
+  "ThreadConcurrency"
+  "ThreadConcurrency.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThreadConcurrency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
